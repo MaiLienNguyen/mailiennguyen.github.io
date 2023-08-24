ca066e57809e3d0548f6018988ce5635fe9068f7
MaiLienNguyen <21522283@gm.uit.edu.vn>
2023-08-24 12:44:04 +0700

    Add file buil

---
+++ build/precache-manifest.2c93e2854789ed226bad9b2f877999e2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ca6a32e89b793b08c2140b761a5f137",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4cefd4114bb270a9d71b",
+    "url": "/static/css/2.3eb2a443.chunk.css"
+  },
+  {
+    "revision": "cee649a1eae08ce1f958",
+    "url": "/static/css/main.0b2451be.chunk.css"
+  },
+  {
+    "revision": "4cefd4114bb270a9d71b",
+    "url": "/static/js/2.fee92e98.chunk.js"
+  },
+  {
+    "revision": "334a3166bc9976dda67650e8c6f88652",
+    "url": "/static/js/2.fee92e98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cee649a1eae08ce1f958",
+    "url": "/static/js/main.1b7b2fd2.chunk.js"
+  },
+  {
+    "revision": "6074ff583ca711c31fe3",
+    "url": "/static/js/runtime-main.1dbfc099.js"
+  },
+  {
+    "revision": "e614c82bd5b8712b3ec0a185d208a943",
+    "url": "/static/media/myProfile.e614c82b.jpg"
+  }
+]);
